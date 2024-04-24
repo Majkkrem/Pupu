da3b2fba4fd8082104a37e525944e515d899479b
Sendula Márk <marksendula@gmail.com>
2024-04-24 22:36:08 +0200

    MainWindow elkezdése (hibás még)

--- Pupu/MainWindow.xaml.cs
+++ Pupu/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,9 +21,49 @@ namespace Pupu
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Window> Windows { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+            Windows = new List<Window>
+           {
+            new Kutya(),
+            new Macska(),
+            new Nyúl(),
+            new Madár()
+
+           };
+       
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (PetComboBox.SelectedItem != null)
+            {
+                string selectedWindowName = ((ComboBoxItem)PetComboBox.SelectedItem).Content.ToString();
+                Window selectedWindow = Windows.Find(w => w.GetType().Name == selectedWindowName);
+
+                if (selectedWindow != null)
+                {
+                    selectedWindow.Show();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Válassz egy állatot a játék megkezdéséhez");
+            }
+        }
+
+        public static string selected;
+        private void Pet_Selection(object sender, SelectionChangedEventArgs e)
+        {
+            selected = (e.AddedItems[0] as ComboBoxItem).Content as string;
+
+
         }
     }
 }
